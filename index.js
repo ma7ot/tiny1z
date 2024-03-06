6463733615fa921b59051dd906ec9c0b36f10e1d
ma7ot <103256298+ma7ot@users.noreply.github.com>
2024-03-06 13:42:57 +0400

    ff

---
+++ index.js
@@ -0,0 +1,97 @@
+const url = 'http://localhost:3000';
+// const url = 'https://tiny1z.com'; // LIVE
+
+export const createSingleUrl = async (data, apiKey) => {
+  try {
+    const response = await fetch(`${url}/api/v1/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: apiKey,
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { error: true, msg: error || 'Failed to fetch data.' };
+  }
+};
+
+export const createBulkUrl = async (data, apiKey) => {
+  try {
+    const response = await fetch(`${url}/api/v1/create/bulk`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: apiKey,
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { error: true, msg: error || 'Failed to fetch data.' };
+  }
+};
+
+export const getUserUrls = async (reference, apiKey) => {
+  try {
+    const response = await fetch(`${url}/api/v1/user?reference=${reference}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: apiKey,
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { error: true, msg: error || 'Failed to fetch data.' };
+  }
+};
+
+export const singleUrlInfo = async (slug, apiKey) => {
+  try {
+    const response = await fetch(`${url}/api/v1/user?slug=${slug}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: apiKey,
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { error: true, msg: error || 'Failed to fetch data.' };
+  }
+};
+
+export const getAllUrls = async (offset, limit, sort, apiKey) => {
+  try {
+    const response = await fetch(`${url}/api/v1/user?offset=${offset}&limit=${limit}&sort=${sort}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: apiKey,
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { error: true, msg: error || 'Failed to fetch data.' };
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    library: 'tiny1z',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
